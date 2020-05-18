b5bca6c83ada23dd140ec40f3487010883ba6abd
mirkela11 <57531634+mirkela11@users.noreply.github.com>
2020-05-18 16:22:35 +0200

    uradjen medicament app, medicament conn, medicament model

--- CaseBased/src/Main.java
+++ CaseBased/src/Main.java
@@ -1,6 +1,19 @@
 import java.util.Arrays;
 
 import cbr.DiagnosisApplication;
+import cbr.MedicamentApplication;import similarity.ListTableSimilarity;
+import ucm.gaia.jcolibri.casebase.LinealCaseBase;
+import ucm.gaia.jcolibri.cbrcore.Attribute;
+import ucm.gaia.jcolibri.cbrcore.CBRCase;
+import ucm.gaia.jcolibri.cbrcore.CBRCaseBase;
+import ucm.gaia.jcolibri.cbrcore.CBRQuery;
+import ucm.gaia.jcolibri.cbrcore.Connector;
+import ucm.gaia.jcolibri.exception.ExecutionException;
+import ucm.gaia.jcolibri.method.retrieve.RetrievalResult;
+import ucm.gaia.jcolibri.method.retrieve.NNretrieval.NNConfig;
+import ucm.gaia.jcolibri.method.retrieve.NNretrieval.NNScoringMethod;
+import ucm.gaia.jcolibri.method.retrieve.NNretrieval.similarity.global.Average;
+import ucm.gaia.jcolibri.method.retrieve.selection.SelectCases;
 
 public class Main {
 
@@ -15,7 +28,11 @@ public class Main {
 //        bladderCbrApplication.run("BAD");
 //        KidneyCbrApplication kidneyCbrApplication = new KidneyCbrApplication();
 //        kidneyCbrApplication.run("cancer tissue small");
-        DiagnosisApplication diagnosisCbrApplication = new DiagnosisApplication();
-        diagnosisCbrApplication.run(Arrays.asList("arm_pain","wrist_pain", "arm_swelling","ankle_pain"));
+       // DiagnosisApplication diagnosisApplication = new DiagnosisApplication();
+       // diagnosisApplication.run(Arrays.asList("arm_pain","wrist_swelling", "arm_stiffness_or_tightness","arm_swelling","hand_or_finger_stiffness_or_tigtness","wrist_stiffnes_or_tightness"));
+        MedicamentApplication medicamentApplication = new MedicamentApplication();
+        medicamentApplication.run(Arrays.asList("ketamine","flumazenil","fluorides","fenoprofen","plasma_protein_fraction"));
+        
+
     }
 }
