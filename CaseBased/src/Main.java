478832e75ca2f80b74da5c59e14e79ea4a267910
nikzejak <nik.zejak@gmail.com>
2020-05-18 16:52:49 +0200

    fix_symptoms

--- CaseBased/src/Main.java
+++ CaseBased/src/Main.java
@@ -2,16 +2,19 @@ import java.util.Arrays;
 
 import cbr.DiagnosisApplication;
 import cbr.MedicamentApplication;
+import cbr.SymptomeApplication;
 
 public class Main {
 
     public static void main(String[] args) {
 //        
-    	DiagnosisApplication diagnosisApplication = new DiagnosisApplication();
-       	diagnosisApplication.run(Arrays.asList("arm_pain","wrist_swelling", "arm_stiffness_or_tightness","arm_swelling","hand_or_finger_stiffness_or_tigtness","wrist_stiffnes_or_tightness"));
-        MedicamentApplication medicamentApplication = new MedicamentApplication();
-        medicamentApplication.run(Arrays.asList("ketamine","flumazenil","fluorides","fenoprofen","plasma_protein_fraction"));
+//    	DiagnosisApplication diagnosisApplication = new DiagnosisApplication();
+//       	diagnosisApplication.run(Arrays.asList("arm_pain","wrist_swelling", "arm_stiffness_or_tightness","arm_swelling","hand_or_finger_stiffness_or_tigtness","wrist_stiffnes_or_tightness"));
+//        MedicamentApplication medicamentApplication = new MedicamentApplication();
+//        medicamentApplication.run(Arrays.asList("ketamine","flumazenil","fluorides","fenoprofen","plasma_protein_fraction"));
         
+        SymptomeApplication symptomeApplication = new SymptomeApplication();
+        symptomeApplication.run(Arrays.asList("bruise","sprain_or_strain","fracture_of_the_arm","injury_to_the_shoulder","spinal_stenosis","brachial_neuritis","pyogenic_skin_infection","carpal_tunel_syndrome"));
 
     }
 }
