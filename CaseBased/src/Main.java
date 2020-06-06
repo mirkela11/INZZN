5e24101318984c33b60a4efde4732db8009d1461
mirkela11 <57531634+mirkela11@users.noreply.github.com>
2020-06-06 14:03:58 +0200

    update Medicaments

--- CaseBased/src/Main.java
+++ CaseBased/src/Main.java
@@ -10,17 +10,23 @@ public class Main {
 
     public static void main(String[] args) {
         
-    	DiagnosisApplication diagnosisApplication = new DiagnosisApplication();
-    	diagnosisApplication.run(Arrays.asList("ankle_pain", "ankle_weakness", "leg_pain"));
+//    	DiagnosisApplication diagnosisApplication = new DiagnosisApplication();
+//    	diagnosisApplication.run(Arrays.asList("ankle_pain", "ankle_weakness", "leg_pain"));
 //		
 //		  MedicamentApplication medicamentApplication = new MedicamentApplication();
-//		  medicamentApplication.run(Arrays.asList("ketamine","flumazenil","fluorides","fenoprofen","plasma_protein_fraction")); // ProceduresCbrApplication pca =
+//		  medicamentApplication.run(Arrays.asList("ketamine","flumazenil","fluorides","fenoprofen","plasma_protein_fraction")); 
+		  // ProceduresCbrApplication pca =
 //		  ProceduresCbrApplication pca = new ProceduresCbrApplication(); 
 //		  pca.run(Arrays.asList("radiographic_imaging_procedure","plain_x-ray",
 //		  "electrocardiogram","kidney_function_tests",
 //		  "intravenous_fluid_replacement","electrolytes_panel",
 //		  "cardiac_enzymes_measurament"));
     	
+    	MedicamentApplication medicamentApplication = new MedicamentApplication();
+		  medicamentApplication.run(Arrays.asList("etodolac","sodium hyaluronate","fluorides","diflunisal","salsalate","flurbiprofen"));
+		  MedicamentApplication medicamentApplication1 = new MedicamentApplication();
+		  medicamentApplication1.run(Arrays.asList("celecoxib","meloxicam","fluorides","cortisone","piroxicam"));
+		  
     }
 
 }
